aeb0f7139a57d2801b0bc53a31e4451bfd539199
haripo <ehutoad@gmail.com>
2016-06-04 22:13:50 +0900

    Improve example

--- example/main.jsx
+++ example/main.jsx
@@ -19,20 +19,6 @@ import Calendar from './../lib/index.js';
 
 
 (() => {
-  var values = {
-    '2016-06-23': 1,
-    '2016-06-26': 2,
-    '2016-06-27': 3,
-    '2016-06-28': 4,
-    '2016-06-29': 4
-  }
-  var until = '2016-06-30';
-
-  var elem = document.getElementById('example1');
-  ReactDOM.render(<Calendar values={values} until={until} />, elem);
-})();
-
-(() => {
   var values = {}
   var until = '2016-12-30';
   var weekNames = ['s', 'm', 't', 'w', 't', 'f', 's'];
